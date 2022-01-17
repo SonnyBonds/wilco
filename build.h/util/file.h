2575945205a3e42dd9032c3fcae23f198689743a
Anders Stenberg <495668+SonnyBonds@users.noreply.github.com>
2022-01-17 15:15:21 +0100

    Make the file writer only write if data is different
    When outputting data it's important to not touch modification
    time of files if nothing changed. The simple file writer now
    compares the incoming data with existing data on disk (if any)
    and only writes the file if it's different. While it's always
    faster to just not generate the data if not needed, this way is
    often much simpler and safer.

--- build.h/util/file.h
+++ build.h/util/file.h
@@ -16,11 +16,39 @@ std::string read(std::filesystem::path path)
     return buffer.str();
 }
 
-void write(std::filesystem::path path, const std::string& data)
+bool write(std::filesystem::path path, const std::string& data)
 {
-    std::filesystem::create_directories(path.parent_path());
+    bool upToDate = false;
+    std::error_code ec;
+    size_t fileSize = 0;
+    std::filesystem::file_size(path, ec);
+    if(!ec && fileSize != data.size())
+    {
+        std::ifstream inputStream(path);
+        std::array<char, 2048> buffer;
+        size_t pos = 0;
+        while(inputStream.good())
+        {
+            size_t chunk = std::min(buffer.size(), fileSize - pos);
+            inputStream.read(buffer.data(), chunk);
+            if(!inputStream.good())
+            {
+                break;
+            }
+            if(memcmp(data.data() + pos, buffer.data(), chunk) != 0)
+            {
+                break;
+            }
+            if(pos == fileSize)
+            {
+                return false;
+            }
+        }
+    }
+
     std::ofstream stream(path);
     stream.write(data.data(), data.size());
+    return true;
 }
 
 }
