7ddcd6c133b4c8d6b80c487f4cec212aa41648ad
Anders Stenberg <495668+SonnyBonds@users.noreply.github.com>
2022-01-21 22:08:31 +0100

    Rework StringId to avoid copy in lookups

--- build.h/core/stringid.h
+++ build.h/core/stringid.h
@@ -10,7 +10,9 @@ struct StringId
     StringId() : _cstr("") {}
     StringId(const StringId& id) = default;
     StringId(const char* id) : StringId(get(id)) {}
-    StringId(const std::string& id) : StringId(get(id.c_str())) {}
+    StringId(const std::string& id) : StringId(get(id)) {}
+    StringId(std::string&& id) : StringId(get(std::move(id))) {}
+    StringId(const std::string_view& id) : StringId(get(id)) {}
 
     bool empty() const
     {
@@ -35,25 +37,103 @@ struct StringId
 private:
     const char* _cstr;
     
-    static std::unordered_set<std::string>& getStorage()
+    // Transparent lookup in unordered_set is a C++20 feature
+    // so we'll have to make do with this... thing.
+    struct StringStorage
     {
-        static std::unordered_set<std::string> storage;
+        StringStorage(std::string str)
+            : str(str)
+        {
+            view = std::string_view(str);
+        }
+
+        StringStorage(std::string_view view)
+            : view(view)
+        {
+        }
+
+        StringStorage(StringStorage&& other)
+        {
+            str = std::move(other.str);
+            view = str;
+        }
+
+        StringStorage(const StringStorage& other) = delete;
+
+        bool operator==(const StringStorage& other) const
+        {
+            return view == other.view;
+        }
+        
+        const char* c_str() const
+        {
+            return str.c_str();
+        }
+
+        std::string str;
+        std::string_view view;
+    };
+
+    struct StringStorageHash
+    {
+        size_t operator()(const StringStorage& storage) const
+        {
+            return std::hash<std::string_view>{}(storage.view);
+        }
+    };
+
+    static std::unordered_set<StringStorage, StringStorageHash>& getStorage()
+    {
+        static std::unordered_set<StringStorage, StringStorageHash> storage;
         return storage;
+    }
+
+    static StringId get(std::string_view str)
+    {
+        if(str.empty())
+        {
+            return StringId();
+        }
+
+        auto& storage = getStorage();
+        auto it = storage.find(str);
+        if(it == storage.end())
+        {
+            it = storage.insert(std::string(str)).first;
+        }
+
+        StringId result;
+        result._cstr = it->c_str();
+        return result;
+    }
+
+    static StringId get(std::string&& str)
+    {
+        if(str.empty())
+        {
+            return StringId();
+        }
+
+        auto& storage = getStorage();
+        auto it = storage.find(str);
+        if(it == storage.end())
+        {
+            it = storage.insert(std::move(str)).first;
+        }
+
+        StringId result;
+        result._cstr = it->c_str();
+        return result;
+    }
+
+    static StringId get(const std::string& str)
+    {
+        return get(std::string_view(str));
     }
 
     static StringId get(const char* str)
     {
-        if(str == nullptr || str[0] == 0)
-        {
-            return StringId();
-        }
-        
-        auto& storage = getStorage();
-        auto entry = storage.insert(str).first;
-
-        StringId result;
-        result._cstr = entry->c_str();
-        return result;
+        return get(std::string_view(str));
     }
 };
 
