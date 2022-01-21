2861ea618745d7ecbd17dc0ec228f6e93db75d54
Anders Stenberg <495668+SonnyBonds@users.noreply.github.com>
2022-01-21 22:11:41 +0100

    Let glob::files not fail for missing directories
    This makes it possible to add folders that may not exist
    now but could be added later, and should then trigger
    a rescan.

--- build.h/util/glob.h
+++ build.h/util/glob.h
@@ -9,17 +9,17 @@ namespace glob
 
 OptionCollection files(const std::filesystem::path& path, const std::vector<std::string>& extensions, bool recurse = true)
 {
-    if(!std::filesystem::exists(path) || !std::filesystem::is_directory(path))
-    {
-        throw std::runtime_error("Source directory '" + path.string() + "' does not exist.");
-    }
-
     OptionCollection result;
     auto& files = result[Files];
     auto generatorDeps = result[GeneratorDependencies];
 
     // Add the directory as a dependency to rescan if the contents change
     generatorDeps += path;
+
+    if(!std::filesystem::exists(path))
+    {
+        return result;
+    }
 
     for(auto entry : std::filesystem::recursive_directory_iterator(path))
     {
